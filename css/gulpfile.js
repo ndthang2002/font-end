e29f1212d85ea0a431835f1c97251a46c4389f94
thang <nguyendinhthang23082002@gmail.com>
2022-12-11 22:17:31 +0700

    WIP css_grid

---
+++ css/gulpfile.js
@@ -0,0 +1,11 @@
+// const browserSync = require('browser-sync');
+var gulp = require('gulp');
+var browserSync =require('browser-sync').create();
+gulp.task('browser-sync',function(){
+    browserSync.init({
+      server:{
+        baseDir:"./"
+      }
+    });
+    gulp.watch(["./*.html", "./css/*.css"]).on('change', browserSync.reload);
+})
